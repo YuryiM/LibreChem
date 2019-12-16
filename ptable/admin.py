4285c0a6dcf29065995058e5c0434d6ca919e9ec
Yuryi M <ymironchyk@gmail.com>
2019-12-16 04:06:26 -0800

    Added basic element model
    Element model created w/ engName, description, atomicNum. Registered to admin.py

--- ptable/admin.py
+++ ptable/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Element
 
-# Register your models here.
+class ElementAdmin(admin.ModelAdmin):
+	list_display = ('engName', 'description', 'atomicNum')
+
+admin.site.register(Element, ElementAdmin)
--- ptable/models.py
+++ ptable/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Element(models.Model):
+	engName = models.CharField(max_length=20)
+	description = models.TextField()
+	atomicNum = models.PositiveSmallIntegerField()
+	'''
+	#Columns to add
+	atomicMass
+	latinName
+	discoverer
+	meltingPointCelsius
+	boilingPointCelsius
+	period
+	group
+	block
+	electronConfiguration
+	atomicRadius
+	electronegativity
+	electronAffinity
+	'''
+
+	def _str_(self):
+		return self.engName
+
+
