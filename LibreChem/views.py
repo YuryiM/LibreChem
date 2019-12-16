bf8a47cb31ddef499daa9b1ba1bafbda02eb8d37
Yuryi M <ymironchyk@gmail.com>
2019-12-16 03:36:35 -0800

    Added home html
    Added home.html file, changed route to go here

--- LibreChem/views.py
+++ LibreChem/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-	return render(request, '../templates/base.html')
+	return render(request, '../templates/home.html')
 
 def about(request):
 	return render(request, '../templates/about.html')
