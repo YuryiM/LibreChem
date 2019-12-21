7e3b75f237ae607fa4d264f88464f52f09ff7b52
Yuryi M <ymironchyk@gmail.com>
2019-12-21 12:31:12 -0800

    Added favicon

--- LibreChem/settings.py
+++ LibreChem/settings.py
@@ -131,10 +131,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static/'),
 )
 
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
+
 
 
 # we whitelist localhost:3000 because that's where frontend will be served
